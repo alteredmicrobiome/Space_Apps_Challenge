a85271324cc063d21c61b6bc2c11fcd418ae76f9
alteredmicrobiome <80645688+alteredmicrobiome@users.noreply.github.com>
2021-10-03 23:27:19 -0500

    Add files via upload

---
+++ SettlementTracker.js
@@ -0,0 +1,35 @@
+/**
+ * Function to mask clouds based on the pixel_qa band of Landsat 8 SR data.
+ * @param {ee.Image} image input Landsat 8 SR image
+ * @return {ee.Image} cloudmasked Landsat 8 image
+ */
+function maskL8sr(image) {
+  // Bits 3 and 5 are cloud shadow and cloud, respectively.
+  var cloudShadowBitMask = (1 << 3);
+  var cloudsBitMask = (1 << 5);
+  // Get the pixel QA band.
+  var qa = image.select('pixel_qa');
+  // Both flags should be set to zero, indicating clear conditions.
+  var mask = qa.bitwiseAnd(cloudShadowBitMask).eq(0)
+                 .and(qa.bitwiseAnd(cloudsBitMask).eq(0));
+  return image.updateMask(mask);
+}
+
+var dataset = ee.ImageCollection('LANDSAT/LC08/C01/T1_SR')
+                  .filterDate('2016-01-01', '2016-12-31')
+                  .map(maskL8sr);
+
+
+var landsat8anp = dataset.median().clip(anp);
+var landsat8lomas = dataset.median().clip(lomas)
+
+var visParams = {
+  bands: ['B4', 'B3', 'B2'],
+  min: 0,
+  max: 3000,
+  gamma: 1.4,
+};
+
+Map.centerObject(anp, 6);
+Map.addLayer(landsat8anp, visParams, "ANP Landsat 8");
+Map.addLayer(landsat8lomas, visParams, "Lomas Landsat 8", false);
